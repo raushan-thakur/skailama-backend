8f7d11dc80d94b2044087a01fd72d36d0ea1361f
Raushan Thakur <raushandtu@gmail.com>
2025-11-10 13:25:02 +0530

    Changes cors error

--- server.js
+++ server.js
@@ -9,7 +9,24 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://skailama-frontend.vercel.app",
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use(morgan("dev"));
 
